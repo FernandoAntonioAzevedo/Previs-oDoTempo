13a64cdf6f482d90d97703ed7f84698e0a71d5d0
Fernando <beratechsolutions@gmail.com>
2023-06-21 00:27:03 -0300

    iniciando correções de exibicao no gitpages

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function colocarNaTela(dados){
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].img-previsao + ".png"
 }
 
 async function buscarCidade(cidade){
